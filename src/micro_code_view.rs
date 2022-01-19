d87f098c4e4e63ebdf39be1a4747bbc237ccbf9d
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-01-19 23:19:03 +0900

    improve micro code view and mem view

--- src/micro_code_view.rs
+++ src/micro_code_view.rs
@@ -5,18 +5,8 @@ pub fn micro_code_view(
     micro_codes: &mut [crate::vm::MicroCode],
 ) {
     const TABLE_NAME: [&str; 12] = [
-        "Address",
-        "X-Bus",
-        "Y-Bus",
-        "ALU",
-        "SFT",
-        "Sin",
-        "FL",
-        "Z-Bus",
-        "Mem",
-        "Branch",
-        "Halt",
-        "MicrocodeAddress",
+        "Address", "X-Bus", "Y-Bus", "ALU", "SFT", "Sin", "FL", "Z-Bus", "Mem", "Branch", "Halt",
+        "B.Addr",
     ];
     eframe::egui::ScrollArea::both().show(ui, |ui| {
         ui.columns(12, |columns| {
@@ -24,26 +14,28 @@ pub fn micro_code_view(
                 columns[x].label(*name);
             }
             for (x, micro_code) in micro_codes.iter_mut().enumerate() {
-                columns[0].add_sized([64.0, 18.0], {
+                columns[0].add_sized([40.0, 18.0], {
                     let addr = micro_code_base_addr + x;
-                    Label::new(RichText::new(format!("{:04X}H", addr)).color(
-                        if addr == micro_code_addr {
-                            Color32::RED
-                        } else {
-                            Color32::WHITE
-                        },
-                    ))
+                    Label::new(
+                        RichText::new(format!("{:04X}H", addr))
+                            .color(if addr == micro_code_addr {
+                                Color32::RED
+                            } else {
+                                Color32::WHITE
+                            })
+                            .monospace(),
+                    )
                 });
-                combo_box_registers_and_switch(&mut columns[1], x * 10, &mut micro_code.x_bus);
-                combo_box_registers_and_switch(&mut columns[2], x * 10 + 1, &mut micro_code.y_bus);
-                combo_box_alu(&mut columns[3], x * 10 + 2, &mut micro_code.alu);
-                combo_box_sft(&mut columns[4], x * 10 + 3, &mut micro_code.sft);
-                combo_box_bool(&mut columns[5], x * 10 + 4, &mut micro_code.sin);
-                combo_box_bool(&mut columns[6], x * 10 + 5, &mut micro_code.fl);
-                combo_box_registers(&mut columns[7], x * 10 + 6, &mut micro_code.z_bus);
-                combo_box_mem(&mut columns[8], x * 10 + 7, &mut micro_code.mem);
-                combo_box_branch(&mut columns[9], x * 10 + 8, &mut micro_code.branch);
-                combo_box_bool(&mut columns[10], x * 10 + 9, &mut micro_code.hlt);
+                columns[1].register_or_switch(&mut micro_code.x_bus, x * 10);
+                columns[2].register_or_switch(&mut micro_code.y_bus, x * 10 + 1);
+                columns[3].alu(&mut micro_code.alu, x * 10 + 2);
+                columns[4].sft(&mut micro_code.sft, x * 10 + 3);
+                columns[5].bool(&mut micro_code.sin, x * 10 + 4);
+                columns[6].bool(&mut micro_code.fl, x * 10 + 5);
+                columns[7].register(&mut micro_code.z_bus, x * 10 + 6);
+                columns[8].mem(&mut micro_code.mem, x * 10 + 7);
+                columns[9].branch(&mut micro_code.branch, x * 10 + 8);
+                columns[10].bool(&mut micro_code.hlt, x * 10 + 9);
                 columns[11].add(eframe::egui::widgets::DragValue::new(&mut micro_code.addr));
             }
         });
@@ -51,10 +43,11 @@ pub fn micro_code_view(
 }
 
 use crate::vm::{AluOp, Branch, MemOp, Register, RegisterOrSwitch, ShiftOp};
-use eframe::egui::{Color32, Label, RichText};
+use eframe::egui::{Color32, Label, Response, RichText, Ui};
 
 const REGISTER_OR_SWITCH_SELECTABLE: [RegisterOrSwitch; 2] =
     [RegisterOrSwitch::Sw1, RegisterOrSwitch::Sw2];
+
 const REGISTER_SELECTABLE: [Register; 13] = [
     Register::Nop,
     Register::R0,
@@ -70,35 +63,7 @@ const REGISTER_SELECTABLE: [Register; 13] = [
     Register::Mar,
     Register::Str,
 ];
-fn combo_box_registers_and_switch(
-    ui: &mut eframe::egui::Ui,
-    id: usize,
-    register: &mut crate::vm::RegisterOrSwitch,
-) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(register.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in REGISTER_OR_SWITCH_SELECTABLE {
-                ui.selectable_value(register, selectable, selectable.to_string());
-            }
-            for selectable in REGISTER_SELECTABLE {
-                ui.selectable_value(
-                    register,
-                    RegisterOrSwitch::Register(selectable),
-                    selectable.to_string(),
-                );
-            }
-        });
-}
-fn combo_box_registers(ui: &mut eframe::egui::Ui, id: usize, register: &mut crate::vm::Register) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(register.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in REGISTER_SELECTABLE {
-                ui.selectable_value(register, selectable, selectable.to_string());
-            }
-        });
-}
+
 const ALU_OP_SELECTABLE: [AluOp; 7] = [
     AluOp::XPlusY,
     AluOp::XMinusY,
@@ -108,15 +73,7 @@ const ALU_OP_SELECTABLE: [AluOp; 7] = [
     AluOp::XPlus1,
     AluOp::XMinus1,
 ];
-fn combo_box_alu(ui: &mut eframe::egui::Ui, id: usize, alu_op: &mut AluOp) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(alu_op.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in ALU_OP_SELECTABLE {
-                ui.selectable_value(alu_op, selectable, selectable.to_string());
-            }
-        });
-}
+
 const SHIFT_OP_SELECTABLE: [ShiftOp; 7] = [
     ShiftOp::Nop,
     ShiftOp::RRwC,
@@ -126,33 +83,9 @@ const SHIFT_OP_SELECTABLE: [ShiftOp; 7] = [
     ShiftOp::Sra,
     ShiftOp::Sla,
 ];
-fn combo_box_sft(ui: &mut eframe::egui::Ui, id: usize, shift_op: &mut ShiftOp) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(shift_op.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in SHIFT_OP_SELECTABLE {
-                ui.selectable_value(shift_op, selectable, selectable.to_string());
-            }
-        });
-}
-fn combo_box_bool(ui: &mut eframe::egui::Ui, id: usize, register: &mut bool) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(if *register { "1" } else { "0" })
-        .show_ui(ui, |ui| {
-            ui.selectable_value(register, false, "0");
-            ui.selectable_value(register, true, "1");
-        });
-}
+
 const MEM_OP_SELECTABLE: [MemOp; 3] = [MemOp::Nop, MemOp::R, MemOp::W];
-fn combo_box_mem(ui: &mut eframe::egui::Ui, id: usize, mem_op: &mut MemOp) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(mem_op.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in MEM_OP_SELECTABLE {
-                ui.selectable_value(mem_op, selectable, selectable.to_string());
-            }
-        });
-}
+
 const BRANCH_OP_SELECTABLE: [Branch; 7] = [
     Branch::Plus1,
     Branch::J,
@@ -162,12 +95,96 @@ const BRANCH_OP_SELECTABLE: [Branch; 7] = [
     Branch::JV,
     Branch::JI,
 ];
-fn combo_box_branch(ui: &mut eframe::egui::Ui, id: usize, branch: &mut Branch) {
-    eframe::egui::ComboBox::from_id_source(id)
-        .selected_text(branch.to_string())
-        .show_ui(ui, |ui| {
-            for selectable in BRANCH_OP_SELECTABLE {
-                ui.selectable_value(branch, selectable, selectable.to_string());
-            }
-        });
+
+// Ui に追加実装.
+trait AdditionalWidget {
+    fn register_or_switch(&mut self, register: &mut RegisterOrSwitch, id: usize) -> Response;
+    fn register(&mut self, register: &mut Register, id: usize) -> Response;
+    fn alu(&mut self, alu_op: &mut AluOp, id: usize) -> Response;
+    fn sft(&mut self, sft_op: &mut ShiftOp, id: usize) -> Response;
+    fn bool(&mut self, x: &mut bool, id: usize) -> Response;
+    fn mem(&mut self, mem_op: &mut MemOp, id: usize) -> Response;
+    fn branch(&mut self, branch_op: &mut Branch, id: usize) -> Response;
+}
+impl AdditionalWidget for Ui {
+    fn register_or_switch(&mut self, register: &mut RegisterOrSwitch, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(register.to_string())
+            .show_ui(self, |ui| {
+                for selectable in REGISTER_OR_SWITCH_SELECTABLE {
+                    ui.selectable_value(register, selectable, selectable.to_string());
+                }
+                for selectable in REGISTER_SELECTABLE {
+                    ui.selectable_value(
+                        register,
+                        RegisterOrSwitch::Register(selectable),
+                        selectable.to_string(),
+                    );
+                }
+            })
+            .response
+    }
+
+    fn register(&mut self, register: &mut Register, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(register.to_string())
+            .show_ui(self, |ui| {
+                for selectable in REGISTER_SELECTABLE {
+                    ui.selectable_value(register, selectable, selectable.to_string());
+                }
+            })
+            .response
+    }
+
+    fn alu(&mut self, alu_op: &mut AluOp, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(alu_op.to_string())
+            .show_ui(self, |ui| {
+                for selectable in ALU_OP_SELECTABLE {
+                    ui.selectable_value(alu_op, selectable, selectable.to_string());
+                }
+            })
+            .response
+    }
+    fn sft(&mut self, shift_op: &mut ShiftOp, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(shift_op.to_string())
+            .show_ui(self, |ui| {
+                for selectable in SHIFT_OP_SELECTABLE {
+                    ui.selectable_value(shift_op, selectable, selectable.to_string());
+                }
+            })
+            .response
+    }
+
+    fn bool(&mut self, register: &mut bool, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(if *register { "1" } else { "" })
+            .show_ui(self, |ui| {
+                ui.selectable_value(register, false, "");
+                ui.selectable_value(register, true, "1");
+            })
+            .response
+    }
+    fn mem(&mut self, mem_op: &mut MemOp, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(mem_op.to_string())
+            .show_ui(self, |ui| {
+                for selectable in MEM_OP_SELECTABLE {
+                    ui.selectable_value(mem_op, selectable, selectable.to_string());
+                }
+            })
+            .response
+    }
+
+    fn branch(&mut self, branch: &mut Branch, id: usize) -> Response {
+        eframe::egui::ComboBox::from_id_source(id)
+            .selected_text(branch.to_string())
+            .show_ui(self, |ui| {
+                for selectable in BRANCH_OP_SELECTABLE {
+                    ui.selectable_value(branch, selectable, selectable.to_string());
+                }
+            })
+            .response
+    }
 }
--- src/ram_view.rs
+++ src/ram_view.rs
@@ -1,3 +1,4 @@
+use eframe::egui::Label;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub fn ram_view(ui: &mut eframe::egui::Ui, memory: &mut [u8]) {
@@ -6,6 +7,7 @@ pub fn ram_view(ui: &mut eframe::egui::Ui, memory: &mut [u8]) {
     let columns = COLUMNS.load(Ordering::Relaxed);
     ui.vertical(|ui| {
         ui.horizontal(|ui| {
+            ui.label("width");
             if ui.button("<").clicked() & (columns > 1) {
                 COLUMNS.store(columns - 1, Ordering::SeqCst);
             }
@@ -14,33 +16,27 @@ pub fn ram_view(ui: &mut eframe::egui::Ui, memory: &mut [u8]) {
                 COLUMNS.store(columns + 1, Ordering::SeqCst);
             }
         });
-        eframe::egui::containers::ScrollArea::both().show(ui, |ui| {
-            ui.vertical(|ui| {
-                if columns > 1 {
-                    for (y, cells) in memory.chunks_mut(columns).enumerate() {
-                        ui.horizontal(|ui| {
-                            ui.label(format!("{:02X}", y * columns));
-                            ui.columns(columns, |columns_ui| {
-                                for (x, cell) in
-                                    cells.iter_mut().zip(columns_ui.iter_mut()).enumerate()
-                                {
-                                    cell.1.add_sized(
-                                        [20.0, 16.0],
-                                        crate::hex_input::HexInput::new(cell.0, y * columns + x),
-                                    );
-                                }
-                            });
-                        });
-                    }
-                } else {
-                    for (y, cell) in memory.iter_mut().enumerate() {
-                        ui.horizontal(|ui| {
-                            ui.label(format!("{:02X}", y));
-                            ui.add(crate::hex_input::HexInput::new(cell, y));
-                        });
-                    }
-                }
+        eframe::egui::containers::ScrollArea::both()
+            .auto_shrink([false, false])
+            .show(ui, |ui| {
+                ui.vertical(|ui| {
+                    ui.columns(columns + 1, |columns_ui| {
+                        for (y, cells) in memory.chunks_mut(columns).enumerate() {
+                            columns_ui[0].add_sized(
+                                [40.0, 18.0],
+                                Label::new(format!("{:02X}", y * columns)),
+                            );
+                            for (x, cell) in cells
+                                .iter_mut()
+                                .zip(columns_ui.iter_mut().skip(1))
+                                .enumerate()
+                            {
+                                cell.1
+                                    .add(crate::hex_input::HexInput::new(cell.0, y * columns + x));
+                            }
+                        }
+                    });
+                });
             });
-        });
     });
 }
